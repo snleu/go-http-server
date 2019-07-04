0f18e01b021eb5b6796da80bb3cfceabd07d27d5
Sarah Leu <sale@netlight.com>
2019-07-04 16:19:45 +0200

    change everything

--- main.go
+++ main.go
@@ -4,21 +4,38 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"sync"
 	"time"
 )
 
+var m sync.RWMutex
+
+// struct to store requests sent in the last 60 seconds
 type DB struct {
 	Requests []time.Time `json:"db"`
 }
 
-func check(err error) {
+//
+func getPersistedRequests(filename string) DB {
+	plan, err := ioutil.ReadFile(filename)
 	if err != nil {
-		panic(err)
+		log.Print(err)
 	}
+
+	var db DB
+	err = json.Unmarshal(plan, &db)
+	if err != nil {
+		// log.error(err)
+		log.Print(err)
+	}
+
+	return db
 }
 
-func cut(times []time.Time, now time.Time) ([]time.Time, int) {
+// count the requests sent within the last 60s
+func countRequests(times []time.Time, now time.Time) int {
 	x := len(times)
 	for x > 0 {
 		x -= 1
@@ -27,33 +44,55 @@ func cut(times []time.Time, now time.Time) ([]time.Time, int) {
 			break
 		}
 	}
-	return times, len(times)
+	return len(times)
+}
+
+func getRequestCount() int {
+	defer m.RUnlock()
+	m.RLock()
+
+	db := getPersistedRequests("./db.json")
+
+	return countRequests(db.Requests, time.Now())
+}
+
+func persistRequests() error {
+	defer m.Unlock()
+	m.Lock()
+
+	db := getPersistedRequests("./db.json")
+
+	// append new request to database
+	db.Requests = append(db.Requests, time.Now())
+
+	encodeDb, err := json.Marshal(db)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile("./db.json", encodeDb, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func incrementRequestCount(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if err := persistRequests(); err != nil {
+			log.Print(err)
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	fmt.Fprintf(w, "count: %d", getRequestCount())
 }
 
 func main() {
-	// load previous timeseries request data on server start up
-	plan, err := ioutil.ReadFile("./db.json")
-	check(err)
-	var db DB
-	err = json.Unmarshal(plan, &db)
-	check(err)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		now := time.Now()
-		db.Requests = append(db.Requests, now)
-		var count int
-		db.Requests, count = cut(db.Requests, now)
-		fmt.Println(db)
-		enCutDb, err := json.Marshal(db)
-
-		fmt.Println(string(enCutDb))
-		check(err)
-		err = ioutil.WriteFile("./db.json", enCutDb, 0644)
-		check(err)
-		w.WriteHeader(200)
-		fmt.Fprintf(w, "count: %d, db.requests: %s", count, enCutDb)
-	})
-
-	http.ListenAndServe(":8080", nil)
-
+	http.Handle("/count", incrementRequestCount(http.HandlerFunc(home)))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
